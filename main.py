71404f8026a57aa884585d94714cfa0993d191f8
Daniel <daniel@petershagen.com>
2024-02-28 07:49:22 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
